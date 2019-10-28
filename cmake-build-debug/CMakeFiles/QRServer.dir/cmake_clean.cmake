71410e88602b857d3b42370670a513fb05175bb6
mario <macastro2@wpi.edu>
2019-10-28 16:01:22 -0400

    Created project

---
+++ cmake-build-debug/CMakeFiles/QRServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QRServer.dir/main.cpp.o"
+  "QRServer"
+  "QRServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QRServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    int opt;
+
+
+
+    return 0;
+}
