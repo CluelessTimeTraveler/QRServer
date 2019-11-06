e64573dca864106748689f7c656e93234513a712
mario <macastro2@wpi.edu>
2019-11-05 20:53:40 -0500

    Server Works, QR works and command line works as far as i Can tell. Code commented. proper protocol implemented. Multiple clients works

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 #include <string.h>
 #include <fstream>
 #include <cstdint>
-
+#import<thread>
+#include <poll.h>
 
 #include <stdexcept>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 using namespace std;
 
 
@@ -20,12 +22,17 @@ using namespace std;
 #define optional_argument 2
 
 std::string getURL(std::string imageName);
+void * handleClient(void * arg);
+int checkTimeout(int new_socket);
+int time_out;
+pthread_mutex_t threadCountLock;
+pthread_mutex_t logLock;
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
 int main(int argc, char *argv[]) {
 
     int opt;
-    int index;
-
 
 
 
@@ -36,7 +43,7 @@ int main(int argc, char *argv[]) {
     int rate_msgs = 3; //requests
     int rate_time = 60; //Seconds
     int max_users = 3;
-    int time_out = 80; //Seconds
+    time_out = 80; //Seconds
 
 
 
@@ -46,22 +53,21 @@ int main(int argc, char *argv[]) {
     //Command Line Args
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const struct option longopts[] = {
-            {"PORT", optional_argument, NULL, 'P'},
+            {"PORT",      optional_argument, NULL, 'P'},
             {"RATE_MSGS", optional_argument, NULL, 'R'},
             {"RATE_TIME", optional_argument, NULL, 'T'},
             {"MAX_USERS", optional_argument, NULL, 'M'},
-            {"TIME_OUT", optional_argument, NULL, 'O'},
-            {0, 0, 0, 0}
+            {"TIME_OUT",  optional_argument, NULL, 'O'},
+            {0, 0, 0,                              0}
     };
 
 
-
-    while(((opt = getopt_long(argc, argv, "PRTMO", longopts, NULL)) != -1)){
+    while (((opt = getopt_long(argc, argv, "PRTMO", longopts, NULL)) != -1)) {
 
         //std::cout << "Arguement is optarg: " << optarg << std::endl;
 
 
-        switch(opt){
+        switch (opt) {
             case 'P':
                 port = atoi(optarg);
                 break;
@@ -90,12 +96,12 @@ int main(int argc, char *argv[]) {
     std::cout << "\nRate_msgs: " << rate_msgs;
     std::cout << "\nRate_time: " << rate_time;
     std::cout << "\nMax_users: " << max_users;
-    std::cout << "\nTime_Out: " << time_out << endl ;
-
+    std::cout << "\nTime_Out: " << time_out << endl;
 
     //Opens socket
-    if((socketDesc = socket(AF_INET, SOCK_STREAM,IPPROTO_TCP)) < 0){
+    if ((socketDesc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
         std::cout << "\nError, unable to create socket";
+        return 1;
 
     }
 
@@ -108,85 +114,153 @@ int main(int argc, char *argv[]) {
 
 
     //Binds Socket
-    if(bind(socketDesc, (struct sockaddr *)&address, sizeof(address)) < 0){
+    if (bind(socketDesc, (struct sockaddr *) &address, sizeof(address)) < 0) {
         std::cout << "\nUnable to bind";
+        return 1;
     }
 
     //Listens on socket
-    if(listen(socketDesc, 3) < 0){
+    if (listen(socketDesc, 3) < 0) {
         std::cout << "\nFailed to listen";
+        return 1;
     }
+
+    std::cout << "\nListening...\n";
 
 
     //Accepts new connection as new_socket
-    if((new_socket = accept(socketDesc, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0){
-        std::cout << "Error accepting";
-    }
+//        if ((new_socket = accept(socketDesc, (struct sockaddr *) &address, (socklen_t *) &addrlen)) < 0) {
+//            std::cout << "Error accepting";
+//        }
+//        std::cout << "Accepted client: " << new_socket;
 
+    int client_sock;
+    pthread_t * threads = (pthread_t *) malloc(sizeof(pthread_t) * max_users);
+    int userCnt = 0;
 
-    //reads in size of QR Code,
-    std::uint32_t buffer;
-    int BR_QRImageSize = read(new_socket, &buffer, sizeof(std::uint32_t));
-    std::cout << "\nRecieved size: " << buffer;
-    if(buffer >= 20000){
-        std::cout << "Client sent file too large. ";
-    }
-
-
-
-    //Creates buffer of needed size for QR Code Image
-    char * recImgBuffer = (char *) malloc(buffer);
-
-    //Reads in and stores QR code image named based on socket FD
-    int BR_QRImage = read(new_socket, recImgBuffer, buffer);
-    FILE * recievedFile;
-    std::string fileName = "imageFromClient" + to_string(new_socket);
-    recievedFile = fopen(fileName.c_str(),"w");
-    fwrite(recImgBuffer, buffer,1,recievedFile);
-    fclose(recievedFile);
-
-
-
-    //Gets URL from QR Code
-    std::string URL = getURL(fileName.c_str());
-
-
-    std::uint32_t returnCode = 0;
-    if(URL == "-1"){
-        std::cout << "\ngetURL returned invalid QRCode";
-        returnCode = 1;
-        URL = "";
-        int BS_returnCode = send(new_socket, &returnCode, sizeof(std::uint32_t), 0);
-        //CONTINUE
-
+    while ((client_sock = accept(socketDesc, (struct sockaddr *) &address, (socklen_t *) &addrlen)) != -1) {
+        if(userCnt < max_users){
+            pthread_create(&threads[userCnt], NULL, handleClient, &client_sock);
+            std::cout << "\nCreated thread: " << userCnt << endl;
+            userCnt++;
+        }
     }
 
 
 
-    int BS_returnCode = send(new_socket, &returnCode, sizeof(std::uint32_t), 0);
-    std::cout << "\nSent return code: " << returnCode;
-
-    //Gets size of URL and sends it.
-    std::uint32_t URLSize=  strlen(URL.c_str());
-    int BS_URLlength = send(new_socket, &URLSize, sizeof(std::uint32_t), 0);
-    std::cout << "\nSent Size of URL: " << strlen(URL.c_str());
-
-    //Sends URL to client
-    int BS_URL = send(new_socket, URL.c_str(), strlen(URL.c_str()), 0);
-    std::cout << "\nSent URL: " << URL;
-
-
-    free(recImgBuffer);
-    close(new_socket);
-
-
-
-
-
-
-
-    return 0;
 }
+#pragma clang diagnostic pop
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+void * handleClient(void * arg){
+
+    int new_socket = *(int*)arg;
+
+    while(1) {
+        //reads in size of QR Code,
+        std::uint32_t buffer;
+
+        std::uint32_t returnCode = 0;
+
+
+
+        if(checkTimeout(new_socket) == 1){
+            returnCode = 2;
+        }
+
+        int BR_QRImageSize = read(new_socket, &buffer, sizeof(std::uint32_t));
+        std::cout << "\nRecieved size: " << buffer;
+        if (buffer >= 20000) {
+            std::cout << "Client sent file too large. ";
+            returnCode = 1;
+        }
+
+
+        if(checkTimeout(new_socket) == 1){
+            returnCode = 2;
+        }
+
+        //Creates buffer of needed size for QR Code Image
+        char *recImgBuffer = (char *) malloc(buffer);
+
+        //Reads in and stores QR code image named based on socket FD
+        int BR_QRImage = read(new_socket, recImgBuffer, buffer);
+        FILE *recievedFile;
+        std::string fileName = "imageFromClient" + to_string(new_socket);
+        recievedFile = fopen(fileName.c_str(), "w");
+        fwrite(recImgBuffer, buffer, 1, recievedFile);
+        fclose(recievedFile);
+
+
+
+        //Gets URL from QR Code
+        std::string URL = getURL(fileName.c_str());
+
+
+
+        if (URL == "-1") {
+            std::cout << "\ngetURL returned invalid QRCode";
+            returnCode = 1;
+            URL = "";
+            int BS_returnCode = send(new_socket, &returnCode, sizeof(std::uint32_t), 0);
+
+
+        }
+
+        switch(returnCode){
+            case 0:
+
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+
+        //Sends return code
+        int BS_returnCode = send(new_socket, &returnCode, sizeof(std::uint32_t), 0);
+        std::cout << "\nSent return code: " << returnCode;
+
+        //Gets size of URL and sends it.
+        std::uint32_t URLSize = strlen(URL.c_str());
+        int BS_URLlength = send(new_socket, &URLSize, sizeof(std::uint32_t), 0);
+        std::cout << "\nSent Size of URL: " << strlen(URL.c_str());
+
+        //Sends URL to client
+        int BS_URL = send(new_socket, URL.c_str(), strlen(URL.c_str()), 0);
+        std::cout << "\nSent URL: " << URL;
+
+
+        free(recImgBuffer);
+
+
+
+    }
+    close(new_socket);
+    return 0;
+
+}
+
+int checkTimeout(int new_socket){
+    struct pollfd pollStruct;
+    pollStruct.fd = new_socket;
+    pollStruct.events = POLLIN;
+    int retval = poll(&pollStruct, 1, time_out * 1000);
+    if (retval == 0){
+        std::cout << "\nTime Out\n";
+        //close(new_socket);
+        //std::cout << "\nThread from: " << new_socket << " closing\n";
+        //pthread_exit(0);
+        return 1;
+    }
+}
+
+
+
+#pragma clang diagnostic pop
 
 std::string getURL(std::string imageName){
 
